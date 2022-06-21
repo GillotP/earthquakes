476963f57760bac3f1affcc8d9fa5232889465fd
Pierre Gillot <well_kept_secret@gmail.com>
2022-06-21 14:01:15 +0200

    added data visualizer

--- exercises_day2.py
+++ exercises_day2.py
@@ -1,15 +1,20 @@
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_data(path):
-	return np.fromfile(path, dtype=np.uint8).reshape([43200, 21600])
+	return np.fromfile(path, dtype=np.uint8).reshape([21600, 43200])
+
+def visualize_data(data, block=bool(1)):
+	fig, ax = plt.subplots(1, 1)
+	ax.imshow(data, aspect="auto")
+	ax.set_xticks([])
+	ax.set_yticks([])
+	plt.show(block=block)
 
 def main():
 	data = load_data("./gl-latlong-1km-landcover.bsq")
+	visualize_data(data)
 
-	# subset = data[::50]
-	print(data)
-	print(data.shape)
 
 if __name__ == '__main__':
 	main()

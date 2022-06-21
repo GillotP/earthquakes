0782f46851488a68afca2c53fddd217e1ecd0d40
Pierre Gillot <well_kept_secret@gmail.com>
2022-06-21 16:07:36 +0200

    added visualization water or land predictor

--- exercises_day2.py
+++ exercises_day2.py
@@ -28,13 +28,17 @@ class EarthMap():
 	def load_data(self, path):
 		self.map = np.fromfile(path, dtype=np.uint8).reshape([21600, 43200])
 
-	def visualize_data(self, subsample=1, block=bool(1)):
+	def set_map_visualization(self, subsample=1):
 		assert type(subsample) == int
 		fig, ax = plt.subplots(1, 1)
 		ax.imshow(self.map[::subsample], aspect="auto", cmap=self.set_maryland_colormap())
 		ax.set_xticks([])
 		ax.set_yticks([])
+		return fig, ax
+
+	def visualize_data(self, block=bool(1)):
 		plt.show(block=block)
+
 
 	def run(self):
 		self.visualize_data(subsample=50)
@@ -42,7 +46,33 @@ class EarthMap():
 	def predict_land_or_water(self, geographic_coordinates):
 		i = np.floor(((90 - geographic_coordinates["N"]) / 180) * (self.map.shape[0] - 1)).astype(int)
 		j = np.floor(((180 + geographic_coordinates["E"]) / 360) * (self.map.shape[1] - 1)).astype(int)
-		print(i, j)
+		water = self.map == 0
+
+		points = np.zeros(shape=self.map.shape, dtype=bool)
+		points[i,j] = True
+
+		water_and_points = np.where(points * water)
+		land_and_points = np.where(points * ~water)
+
+
+
+		# print(i)
+		# print(j)
+		# print(water[i, j])
+		# print(np.where(water_and_points))
+		# print(np.where(land_and_points))
+
+		fig, ax = self.set_map_visualization()
+		#ax.scatter(j, i, marker="o", c="w")
+		ax.scatter(water_and_points[1], water_and_points[0], marker="o", c="k", label="Water")
+		ax.scatter(land_and_points[1], land_and_points[0], marker="x", c="k", label="Land")
+		fig.legend()
+
+		self.visualize_data()
+
+		exit()
+
+		return water[i, j]
 
 
 class EarthquakesRecord():
